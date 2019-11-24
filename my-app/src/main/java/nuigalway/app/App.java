a8987f65f39d12e2d3f39b755ee79301563ae5ca
Bernard Kavanagh <B.Kavanagh8@nuigalway.ie>
2019-11-24 23:20:48 +0000

    Added three classes; Student, Module and Course. Edited main() in App.java

--- my-app/src/main/java/nuigalway/app/App.java
+++ my-app/src/main/java/nuigalway/app/App.java
@@ -9,5 +9,29 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        Student student1 = new Student();
+        Student student2 = new Student();
+        Student student3 = new Student();
+        
+        Module module1 = new Module();
+        Module module2 = new Module();
+
+        Course course = new Course();
+        
+        module1.add(student1);
+        module1.add(student2);
+        
+        module2.add(student2);
+        module2.add(student3);
+        
+        course.add(module1);
+        course.add(module2);
+        
+        student1.getUsername();
+        
+        module1.getStudents();
+        
+        
     }
 }
